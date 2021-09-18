b4e46bc5075e8aa3689bc493b5496b76d3406961
PercyGB <kuznetsovalv@yandex.ru>
2021-09-18 07:42:21 +0400

    version 3.0

--- src/test/java/cloud/autotests/tests/SberBankTests.java
+++ src/test/java/cloud/autotests/tests/SberBankTests.java
@@ -1,14 +1,19 @@
 package cloud.autotests.tests;
 
+import cloud.autotests.annotations.Layer;
+import cloud.autotests.annotations.Microservice;
 import cloud.autotests.helpers.DriverUtils;
 import cloud.autotests.steps.WebSteps;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import cloud.autotests.annotations.JiraIssue;
+import cloud.autotests.annotations.JiraIssues;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -18,6 +23,7 @@ import static org.junit.jupiter.params.ParameterizedTest.*;
 
 // "https://jenkins.autotests.cloud/job/c06-liakuz-lesson12/"
 
+@Layer("web")
 public class SberBankTests extends TestBase {
 
     WebSteps steps = new WebSteps();
@@ -25,6 +31,8 @@ public class SberBankTests extends TestBase {
     @Test
     @Tag("Smoke")
     @DisplayName("Проверка заголовка страницы")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Owner("ivanovi")
     void titleTest() {
         steps.openMainPage();
 
@@ -39,6 +47,8 @@ public class SberBankTests extends TestBase {
     @Test
     @Tag("Smoke")
     @DisplayName("Проверка консоли браузера на наличие ошибок")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Owner("ivanovi")
     void consoleShouldNotHaveErrorsTest() {
         steps.openMainPage();
 
@@ -53,6 +63,9 @@ public class SberBankTests extends TestBase {
     @Test
     @Feature("Калькулятор вкладов")
     @DisplayName("Проверка мининимального значения величины вклада")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Microservice("Deposit")
+    @Owner("luznetsoval")
     void minValueOfDepositAmountTest(){
 
         steps.openMainPage();
@@ -71,7 +84,9 @@ public class SberBankTests extends TestBase {
     @Feature("Калькулятор вкладов")
     @ParameterizedTest(name = DISPLAY_NAME_PLACEHOLDER + ": " + ARGUMENTS_WITH_NAMES_PLACEHOLDER)
     @CsvSource({"100000, 3 401,91", "300000, 10 205,74", "500000, 17 783,46"})
-
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Microservice("Deposit")
+    @Owner("luznetsoval")
     void depositIncomeValueTest(String depositAmount,
                                 String depositIncome) {
 
@@ -86,6 +101,9 @@ public class SberBankTests extends TestBase {
     @CsvSource({"100000, 3,4", "500000, 3,56", "1000000, 3,56"})
     @DisplayName("Проверка величины процента по вкладу")
     @Feature("Калькулятор вкладов")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Microservice("Deposit")
+    @Owner("luznetsoval")
     void depositRateValueTests(String depositAmount,
                                String depositRate) {
         steps.openMainPage();
@@ -98,6 +116,8 @@ public class SberBankTests extends TestBase {
     @Test
     @DisplayName("Проверка работы ссылки \"Все вопросы и ответы\"")
     @Feature("Навигация по ссылкам")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Owner("ivanovi")
     void qAndALinkTest(){
         steps.openMainPage();
         steps.clickToLink("Все вопросы и ответы");
@@ -107,6 +127,8 @@ public class SberBankTests extends TestBase {
     @Test
     @DisplayName("Проверка работы ссылки \"Все новости\"")
     @Feature("Навигация по ссылкам")
+    @JiraIssues({@JiraIssue("HOM-236")})
+    @Owner("ivanovi")
     void allNewsLinkTest(){
         steps.openMainPage();
         steps.clickToLink("Все новости");
